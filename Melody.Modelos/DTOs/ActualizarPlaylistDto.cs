b42a5400625495447b1f68799a181a8e04273ad7
JohnRamos10 <johnescanor77@gmail.com>
2025-07-15 12:09:49 -0500

    Agrego o creo mi clase ActualizarPlayListDto

---
+++ Melody.Modelos/DTOs/ActualizarPlaylistDto.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Melody.Modelos.DTOs
+{
+    public class ActualizarPlaylistDto
+    {
+        [StringLength(100, ErrorMessage = "El nombre no puede exceder los 100 caracteres")]
+        public string? Nombre { get; set; }
+
+        public bool? EsPublica { get; set; }
+
+        public IFormFile? Imagen { get; set; }
+    }
+}
