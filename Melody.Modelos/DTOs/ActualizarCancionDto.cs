c16ccad500f09d429c99ece2f645895a34250427
JohnRamos10 <johnescanor77@gmail.com>
2025-07-15 12:06:39 -0500

    Agrego o creo mi clase ActualizarCancion

---
+++ Melody.Modelos/DTOs/ActualizarCancionDto.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Melody.Modelos.DTOs
+{
+    public class ActualizarCancionDto
+    {
+        public string? Titulo { get; set; } = string.Empty;
+        public int GeneroId { get; set; }
+
+        public int? AlbumId { get; set; }
+        public DateTime FechaLanzamiento { get; set; } = DateTime.Now;
+        public IFormFile? ArchivoAudio { get; set; } = null!;
+        public IFormFile? ImagenPortada { get; set; }
+    }
+}
