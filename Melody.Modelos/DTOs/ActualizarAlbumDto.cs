02243d0f8b7cd3a8e24c1182c03c136f87779ae7
JohnRamos10 <johnescanor77@gmail.com>
2025-07-15 12:03:41 -0500

    Agrego mi clase ActualizarAlbumDto y de paso instalo el paquete necesario para mi atributo IFormFile

---
+++ Melody.Modelos/DTOs/ActualizarAlbumDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace Melody.Modelos.DTOs
+{
+    public class ActualizarAlbumDto
+    {
+        public string? Titulo { get; set; }
+        public int GeneroId { get; set; }
+        public IFormFile? Portada { get; set; }
+    }
+}
