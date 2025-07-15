82af4863885307ce5ea38f05d010db002d4fe94e
JohnRamos10 <johnescanor77@gmail.com>
2025-07-15 12:16:17 -0500

    agrego mi clase ActualizarPerfilUsuarioDto

---
+++ Melody.Modelos/DTOs/ActualizarPerfilUsuarioDto.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Melody.Modelos.DTOs
+{
+    public class ActualizarPerfilUsuarioDto
+    {
+        public string? Nombre { get; set; }
+        public string? Apellido { get; set; }
+        public IFormFile? FotoPerfil { get; set; }
+    }
+}
