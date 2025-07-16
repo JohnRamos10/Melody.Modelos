3dceddf9cef0042180bcb0c8e1a3600579943a32
JohnRamos10 <johnescanor77@gmail.com>
2025-07-15 20:17:03 -0500

    agrego una clase para crear la suscripcion y ademas un atributo de validacion

---
+++ Melody.Modelos/DTOs/CrearSuscripcionDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Melody.Modelos.DTOs
+{
+    public class CrearSuscripcionDto
+    {
+        [Required(ErrorMessage = "El ID del plan es requerido")]
+        public int PlanId { get; set; }
+    }
+}
