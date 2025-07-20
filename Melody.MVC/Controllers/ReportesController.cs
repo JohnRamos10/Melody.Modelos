f57f8f6b601e7976d7e67f2796a743002dd37861
JohnRamos10 <johnescanor77@gmail.com>
2025-07-19 23:00:28 -0500

    creo mi reporte controller para poder generar el pdf

---
+++ Melody.MVC/Controllers/ReportesController.cs
@@ -0,0 +1,25 @@
+﻿using Melody.API.Consumer;
+using Melody.Modelos;
+using Melody.MVC.Reports;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Melody.MVC.Controllers
+{
+    public class ReportesController : Controller
+    {
+        public async Task<IActionResult> GenerarReportePagos()
+        {
+            // Obtener pagos desde la API con Crud<T>
+            List<Pago> pagos = await Crud<Pago>.GetAll();
+
+            if (pagos == null) pagos = new List<Pago>();
+
+            // Generar el PDF con QuestPDF
+            byte[] pdfBytes = ReportePDF.Generar(pagos);
+
+            // Devolver el archivo PDF al navegador para descargar
+            return File(pdfBytes, "application/pdf", "reporte-pagos.pdf");
+        }
+    }
+}
